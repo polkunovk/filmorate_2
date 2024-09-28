22f1122067c4486d5bc117c953bfb47ce2c75ee9
Konstantin <lenaaks@mail.ru>
2024-09-29 00:51:49 +0300

    а

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-//import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,30 +25,36 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+        log.info("Добавление пользователя: {}", user);
         User createdUser = userService.addUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);  // Статус 201 Created
     }
 
     @PutMapping
     public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
+        log.info("Обновление пользователя с ID: {}", user.getId());
         try {
             User updatedUser = userService.updateUser(user);
             return ResponseEntity.ok(updatedUser);
         } catch (IllegalArgumentException e) {
+            log.error("Ошибка при обновлении пользователя с ID: {}", user.getId());
             throw new ValidationException("Пользователь с таким ID не найден.");
         }
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
+        log.info("Запрос всех пользователей");
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable int id) {
+        log.info("Запрос пользователя с ID: {}", id);
         User user = userService.getUserById(id);
         if (user == null) {
+            log.error("Пользователь с ID {} не найден", id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);  // 404 Not Found
         }
         return ResponseEntity.ok(user);
@@ -57,31 +62,35 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        log.info("Удаление пользователя с ID: {}", id);
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();  // Статус 204 No Content
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     public ResponseEntity<Void> addFriend(@PathVariable int id, @PathVariable int friendId) {
-        log.info("Попытка добавить друга: пользователь {} добавляет {} в друзья", id, friendId);
+        log.info("Добавление друга: пользователь {} добавляет {} в друзья", id, friendId);
         userService.addFriend(id, friendId);
-        return ResponseEntity.status(HttpStatus.CREATED).build();  // 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).build();  // Статус 201 Created
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     public ResponseEntity<Void> removeFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Удаление друга: пользователь {} удаляет {} из друзей", id, friendId);
         userService.removeFriend(id, friendId);
-        return ResponseEntity.noContent().build();  // 204 No Content
+        return ResponseEntity.noContent().build();  // Статус 204 No Content
     }
 
     @GetMapping("/{id}/friends")
     public ResponseEntity<List<User>> getUserFriends(@PathVariable int id) {
+        log.info("Запрос друзей пользователя с ID: {}", id);
         List<User> friends = userService.getFriends(id);
         return ResponseEntity.ok(friends);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public ResponseEntity<List<User>> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        log.info("Запрос общих друзей между пользователем {} и пользователем {}", id, otherId);
         List<User> commonFriends = userService.getCommonFriends(id, otherId);
         return ResponseEntity.ok(commonFriends);
     }
