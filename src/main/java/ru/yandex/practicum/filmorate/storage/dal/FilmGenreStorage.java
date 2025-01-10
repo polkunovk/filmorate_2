4507aeb842dddda808eb05debdc04b5be5713bc2
Konstantin <polkunovk@yandex.ru>
2025-01-11 00:42:28 +0300

    12 спринт испр

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmGenreStorage.java
@@ -11,8 +11,7 @@ import java.util.List;
 public class FilmGenreStorage extends BaseStorage<FilmGenre> {
     private static final String FIND_ALL_QUERY = "SELECT * FROM FILM_GENRE";
     private static final String FIND_BY_FILM_ID_QUERY = "SELECT * FROM FILM_GENRE WHERE FILM_ID = ?";
-    private static final String INSERT_QUERY = "INSERT INTO FILM_GENRE (FILM_ID, GENRE_ID)" +
-            "VALUES (?, ?)";
+    private static final String INSERT_QUERY = "INSERT INTO FILM_GENRE (FILM_ID, GENRE_ID) VALUES (?, ?)";
     private static final String DELETE_QUERY = "DELETE FROM FILM_GENRE WHERE FILM_ID = ? AND GENRE_ID = ?";
 
     public FilmGenreStorage(JdbcTemplate jdbc, RowMapper<FilmGenre> mapper) {
@@ -24,7 +23,6 @@ public class FilmGenreStorage extends BaseStorage<FilmGenre> {
     }
 
     public FilmGenre addGenre(FilmGenre genre) {
-
         Integer id = insert(INSERT_QUERY, genre.getFilmId(), genre.getGenreId());
         genre.setId(id);
         return genre;
