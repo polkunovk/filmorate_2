5457d88fcde2d3ca81e0227e8ac41246ce7fef71
Konstantin <polkunovk@yandex.ru>
2025-01-11 23:19:14 +0300

    ErrorHandler Добавлено логирование ошибок

--- src/main/java/ru/yandex/practicum/filmorate/service/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,33 +15,40 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 @RestControllerAdvice
 public class ErrorHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDetails handleNotFound(NotFoundException ex) {
+        log.error("Данные не найдены: {}", ex.getMessage(), ex);
         return new ErrorDetails(ex.getMessage());
     }
 
     @ExceptionHandler(InternalServerException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDetails handleInternalServerException(InternalServerException ex) {
+        log.error("Ошибка сервера: {}", ex.getMessage(), ex);
         return new ErrorDetails(ex.getMessage());
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDetails handleValidationException(ValidationException ex) {
+        log.error("Ошибка валидации: {}", ex.getMessage(), ex);
         return new ErrorDetails(ex.getMessage());
     }
 
     @ExceptionHandler(ConditionsNotMetException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ErrorDetails handleConditionsNotMetException(ConditionsNotMetException ex) {
+        log.error("Условия не выполнены: {}", ex.getMessage(), ex);
         return new ErrorDetails(ex.getMessage());
     }
 
     @ExceptionHandler(DuplicatedDataException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorDetails handleDuplicatedDataException(DuplicatedDataException ex) {
+        log.error("Данные дублируются: {}", ex.getMessage(), ex);
         return new ErrorDetails(ex.getMessage());
     }
 
